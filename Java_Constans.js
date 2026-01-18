cd2b7bebea5a5b016b7d55aa56e574066af0d241
Yosif-PC <yosifmohamed.pc@gmail.com>
2026-01-18 19:06:18 +0200

    Add Save_content_From_URL function for localStorage

--- Java_Constans.js
+++ Java_Constans.js
@@ -16,4 +16,31 @@ fetch(Image_URL)
     });
 
 }
+
+
+function Save_content_From_URL(Data_URL, Data_Name){
+    if (localStorage.getItem(Data_Name)) return;
+
+fetch(Data_URL)
+    .then(res => res.blob())
+    .then(blob => {
+        const reader = new FileReader();
+        reader.onload = () => {
+            localStorage.setItem(Data_Name, reader.result); // حفظ فقط
+            console.log("تم حفظ  فقط ✅");
+        };
+        reader.readAsDataURL(blob);
+    })
+    .catch(err => {
+        console.error("فشل تحميل  ❌", err);
+    });
+
+
+}
+
+
+Save_content_From_URL("https://raw.githubusercontent.com/Yosif-PC/MY_CSS/main/style.css","A1");
+
+
 Save_image_From_URL("https://raw.githubusercontent.com/Yosif-PC/MY_CSS/main/Home_Logo.png","My_Logo");
+
