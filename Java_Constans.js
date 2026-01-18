802eff13c1f6378018dab9413b9f200e4435958d
Yosif-PC <yosifmohamed.pc@gmail.com>
2026-01-18 19:09:59 +0200

    Update Java_Constans.js

--- Java_Constans.js
+++ Java_Constans.js
@@ -22,25 +22,19 @@ function Save_content_From_URL(Data_URL, Data_Name){
     if (localStorage.getItem(Data_Name)) return;
 
 fetch(Data_URL)
-    .then(res => res.blob())
-    .then(blob => {
-        const reader = new FileReader();
-        reader.onload = () => {
-            localStorage.setItem(Data_Name, reader.result); // حفظ فقط
-            console.log("تم حفظ  فقط ✅");
-        };
-        reader.readAsDataURL(blob);
+    .then(res => res.text())
+    .then(Content => {
+        localStorage.setItem(Data_Name, Content); // حفظ فقط
+        console.log("تم حفظ ملف  بنجاح ✅");
     })
     .catch(err => {
-        console.error("فشل تحميل  ❌", err);
+        console.error("فشل تحميل ملف ❌", err);
     });
-
-
 }
 
 
-Save_content_From_URL("https://raw.githubusercontent.com/Yosif-PC/MY_CSS/main/style.css","A1");
 
+Save_content_From_URL("https://raw.githubusercontent.com/Yosif-PC/MY_CSS/main/Home_Logo.png","Home_CSS");
 
 Save_image_From_URL("https://raw.githubusercontent.com/Yosif-PC/MY_CSS/main/Home_Logo.png","My_Logo");
 
